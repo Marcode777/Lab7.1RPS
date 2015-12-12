2424ae6b5981bc83523becaa75ec23d43ce957ee
Marcode777 <marc102202@hotmail.com>
2015-12-12 13:58:04 -0500

    added score sections

--- js/RPS.js
+++ js/RPS.js
@@ -1,4 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function(){
+    var ties = 0;
+    var compWins = 0;
+    var youWin = 0;
     var myChoices = ["rock", "paper", "scissors"];
     var compChoice = null;
     var myChoice = null;
@@ -11,31 +14,43 @@ $(document).ready(function() {
       $("#myChoice").html(myChoice);
 // be sure to use == for comparisons, and also === 
     if (myChoice == "rock" && compChoice == "rock"){
-       results ="draw";
+       results ="draw"; 
+       ties++;
+       alert('ties '+ ties);
     }
     if (myChoice == "rock" && compChoice == "paper"){
       results ="Computer Wins";
+       compWins++;
     }
     if (myChoice == "rock" && compChoice == "scissors"){
       results ="You Win";
+      youWin++;
     }
     if (myChoice == "paper" && compChoice == "paper"){
       results ="draw";
+      ties++;
+      alert('ties '+ ties);
     }
     if (myChoice == "paper" && compChoice == "rock"){
       results ="You Win";
+      youWin++;
     }
     if (myChoice == "paper" && compChoice == "scissors"){
       results ="Computer Wins";
+      compWins++;
     }
     if (myChoice == "scissors" && compChoice == "scissors"){
       results ="draw";
+      ties++;
+      alert('ties '+ ties);
     }
     if (myChoice == "scissors" && compChoice == "paper"){
       results ="You Win";
+      youWin++;
     }
     if (myChoice == "scissors" && compChoice == "rock"){
       results ="Computer Wins";
+      compWins++;
     }
     $("#Results").html("You chose " + myChoice + " computer chose " + compChoice + " Result; " + results);
   });
@@ -43,6 +58,9 @@ $(document).ready(function() {
 
   
 
+
+
+  
 
 
 
